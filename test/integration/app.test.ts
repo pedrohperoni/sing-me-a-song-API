71d4ee2d01caf974eaed518806c2d831d1ada3e7
pedro <pedrohperoni@gmail.com>
2022-05-09 09:03:39 -0300

    STYLE: Prettier Formatting

--- test/integration/app.test.ts
+++ test/integration/app.test.ts
@@ -1,140 +1,156 @@
-import app from "../../src/app.js";
-import supertest from "supertest";
-import { prisma } from "../../src/database.js";
+import app from '../../src/app.js'
+import supertest from 'supertest'
+import { prisma } from '../../src/database.js'
 import {
-  createRecommendationBody,
-  createRecommendation,
-  createRecommendationWithCustomScore,
-} from "../factories/RecommendationFactory.js";
+    createRecommendationBody,
+    createRecommendation,
+    createRecommendationWithCustomScore,
+} from '../factories/RecommendationFactory.js'
 
-describe("INTEGRATION TESTING", () => {
-  beforeAll(truncateRecommendations);
-  afterAll(truncateRecommendations);
-  afterAll(disconnect);
+describe('INTEGRATION TESTING', () => {
+    beforeAll(truncateRecommendations)
+    afterAll(truncateRecommendations)
+    afterAll(disconnect)
 
-  // --------------------------- INSERT RECOMMENDATION---------------------------
+    // --------------------------- INSERT RECOMMENDATION---------------------------
 
-  describe("Create Recommendation - POST /recommendation ", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
+    describe('Create Recommendation - POST /recommendation ', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
 
-    it("should return status 201 when a valid body is given, and persist the video", async () => {
-      const body = await createRecommendationBody();
-      const response = await supertest(app).post("/recommendations").send(body);
-      expect(response.status).toBe(201);
-    });
+        it('should return status 201 when a valid body is given, and persist the video', async () => {
+            const body = await createRecommendationBody()
+            const response = await supertest(app)
+                .post('/recommendations')
+                .send(body)
+            expect(response.status).toBe(201)
+        })
 
-    it("should return status 422 when a wrong schema is requested (testing joi)", async () => {
-      const body = {};
-      const response = await supertest(app).post("/recommendations").send(body);
-      expect(response.status).toBe(422);
-    });
-  });
+        it('should return status 422 when a wrong schema is requested (testing joi)', async () => {
+            const body = {}
+            const response = await supertest(app)
+                .post('/recommendations')
+                .send(body)
+            expect(response.status).toBe(422)
+        })
+    })
 
-  // --------------------------- INCREMENT RECOMMENDATION SCORE---------------------------
+    // --------------------------- INCREMENT RECOMMENDATION SCORE---------------------------
 
-  describe("Upvote Recommendation - POST /recommendation/:id/upvote", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
-    it("should return status 200 when a upvote is successfully sent, and persist the updated score", async () => {
-      const newRecommendation = await createRecommendation();
-      const originalRecommendation = await prisma.recommendation.findUnique({
-        where: { name: newRecommendation.name },
-      });
-      const response = await supertest(app).post(
-        `/recommendations/${originalRecommendation.id}/upvote`
-      );
-      const updatedRecommendation = await prisma.recommendation.findUnique({
-        where: { id: originalRecommendation.id },
-      });
-      expect(response.status).toBe(200);
-      expect(updatedRecommendation.score).toBe(originalRecommendation.score + 1);
-    });
-  });
+    describe('Upvote Recommendation - POST /recommendation/:id/upvote', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
+        it('should return status 200 when a upvote is successfully sent, and persist the updated score', async () => {
+            const newRecommendation = await createRecommendation()
+            const originalRecommendation =
+                await prisma.recommendation.findUnique({
+                    where: { name: newRecommendation.name },
+                })
+            const response = await supertest(app).post(
+                `/recommendations/${originalRecommendation.id}/upvote`
+            )
+            const updatedRecommendation =
+                await prisma.recommendation.findUnique({
+                    where: { id: originalRecommendation.id },
+                })
+            expect(response.status).toBe(200)
+            expect(updatedRecommendation.score).toBe(
+                originalRecommendation.score + 1
+            )
+        })
+    })
 
-  // --------------------------- DECREASE RECOMMENDATION SCORE---------------------------
+    // --------------------------- DECREASE RECOMMENDATION SCORE---------------------------
 
-  describe("Downvote Recommendation - POST /recommendation/:id/upvote", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
-    it("should return status 200 when a downvote is successfully sent, and persist the updated score", async () => {
-      const newRecommendation = await createRecommendation();
-      const originalRecommendation = await prisma.recommendation.findUnique({
-        where: { name: newRecommendation.name },
-      });
-      const response = await supertest(app).post(
-        `/recommendations/${originalRecommendation.id}/downvote`
-      );
-      const updatedRecommendation = await prisma.recommendation.findUnique({
-        where: { id: originalRecommendation.id },
-      });
-      expect(response.status).toBe(200);
-      expect(updatedRecommendation.score).toBe(originalRecommendation.score - 1);
-    });
-  });
+    describe('Downvote Recommendation - POST /recommendation/:id/upvote', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
+        it('should return status 200 when a downvote is successfully sent, and persist the updated score', async () => {
+            const newRecommendation = await createRecommendation()
+            const originalRecommendation =
+                await prisma.recommendation.findUnique({
+                    where: { name: newRecommendation.name },
+                })
+            const response = await supertest(app).post(
+                `/recommendations/${originalRecommendation.id}/downvote`
+            )
+            const updatedRecommendation =
+                await prisma.recommendation.findUnique({
+                    where: { id: originalRecommendation.id },
+                })
+            expect(response.status).toBe(200)
+            expect(updatedRecommendation.score).toBe(
+                originalRecommendation.score - 1
+            )
+        })
+    })
 
-  // --------------------------- GET RANDOM RECOMMENDATION---------------------------
+    // --------------------------- GET RANDOM RECOMMENDATION---------------------------
 
-  describe("Get a random recommendation - GET /recommendations/random", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
-    it("should return a random recommendation already registered in the database", async () => {
-      const recommendation = await createRecommendation();
-      const response = await supertest(app).get("/recommendations/random");
-      expect(response.body).toEqual(recommendation);
-    });
-  });
+    describe('Get a random recommendation - GET /recommendations/random', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
+        it('should return a random recommendation already registered in the database', async () => {
+            const recommendation = await createRecommendation()
+            const response = await supertest(app).get('/recommendations/random')
+            expect(response.body).toEqual(recommendation)
+        })
+    })
 
-  // --------------------------- GET ALL RECOMMENDATIONS---------------------------
+    // --------------------------- GET ALL RECOMMENDATIONS---------------------------
 
-  describe("Get all recommendations - GET /recommendations", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
-    it("should return all recommendations already registered in the database", async () => {
-      const recommendation = await createRecommendation();
-      const response = await supertest(app).get("/recommendations");
-      expect(response.body).toEqual([recommendation]);
-    });
-  });
+    describe('Get all recommendations - GET /recommendations', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
+        it('should return all recommendations already registered in the database', async () => {
+            const recommendation = await createRecommendation()
+            const response = await supertest(app).get('/recommendations')
+            expect(response.body).toEqual([recommendation])
+        })
+    })
 
-  // --------------------------- GET TOP RECOMMENDATIONS---------------------------
+    // --------------------------- GET TOP RECOMMENDATIONS---------------------------
 
-  describe("Get top (amount:3) recommendations by descending score - GET /recommendations/top/:amount", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
-    it("should return the top(amount) recommendations sorted by score (descending)", async () => {
-      const firstRecommendation = await createRecommendationWithCustomScore(100);
-      const lastRecommendation = await createRecommendationWithCustomScore(2);
-      const secondRecommendation = await createRecommendationWithCustomScore(50);
-      const response = await supertest(app).get("/recommendations/top/3");
-      expect(response.body[0]).toEqual(firstRecommendation);
-      expect(response.body[1]).toEqual(secondRecommendation);
-      expect(response.body[2]).toEqual(lastRecommendation);
-    });
-  });
+    describe('Get top (amount:3) recommendations by descending score - GET /recommendations/top/:amount', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
+        it('should return the top(amount) recommendations sorted by score (descending)', async () => {
+            const firstRecommendation =
+                await createRecommendationWithCustomScore(100)
+            const lastRecommendation =
+                await createRecommendationWithCustomScore(2)
+            const secondRecommendation =
+                await createRecommendationWithCustomScore(50)
+            const response = await supertest(app).get('/recommendations/top/3')
+            expect(response.body[0]).toEqual(firstRecommendation)
+            expect(response.body[1]).toEqual(secondRecommendation)
+            expect(response.body[2]).toEqual(lastRecommendation)
+        })
+    })
 
-  // --------------------------- GET RECOMMENDATION BY ID---------------------------
+    // --------------------------- GET RECOMMENDATION BY ID---------------------------
 
-  describe("Get recommendation by id - GET /recommendations/:id", () => {
-    beforeAll(truncateRecommendations);
-    afterAll(disconnect);
-    it("should return the correct recommendation ", async () => {
-      const recommendation = await createRecommendation();
-      const createdRecommendation = await prisma.recommendation.findUnique({
-        where: { name: recommendation.name },
-      });
-      const response = await supertest(app).get(
-        `/recommendations/${createdRecommendation.id}`
-      );
-      expect(response.body).toEqual(recommendation);
-    });
-  });
-});
+    describe('Get recommendation by id - GET /recommendations/:id', () => {
+        beforeAll(truncateRecommendations)
+        afterAll(disconnect)
+        it('should return the correct recommendation ', async () => {
+            const recommendation = await createRecommendation()
+            const createdRecommendation =
+                await prisma.recommendation.findUnique({
+                    where: { name: recommendation.name },
+                })
+            const response = await supertest(app).get(
+                `/recommendations/${createdRecommendation.id}`
+            )
+            expect(response.body).toEqual(recommendation)
+        })
+    })
+})
 
 async function truncateRecommendations() {
-  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations`
 }
 
 async function disconnect() {
-  await prisma.$disconnect();
+    await prisma.$disconnect()
 }
--- test/unit/recommendations.test.ts
+++ test/unit/recommendations.test.ts
@@ -1,122 +1,128 @@
-import { jest } from "@jest/globals";
-import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
-import { recommendationService } from "../../src/services/recommendationsService.js";
-import { createMockRecommendation } from "../factories/RecommendationFactory.js";
+import { jest } from '@jest/globals'
+import { recommendationRepository } from '../../src/repositories/recommendationRepository.js'
+import { recommendationService } from '../../src/services/recommendationsService.js'
+import { createMockRecommendation } from '../factories/RecommendationFactory.js'
 
-describe("UNIT TESTING", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    jest.resetAllMocks();
-  });
-
-  // --------------------------- INSERT RECOMMENDATION---------------------------
-
-  describe("Create Recommendation - POST /recommendation ", () => {
-    it("should return status 409(conflict) when an recommendation name is already registered (unique)", async () => {
-      const recommendation = await createMockRecommendation(0);
-      const body = {
-        name: recommendation.name,
-        youtubeLink: recommendation.youtubeLink,
-      };
-      jest
-        .spyOn(recommendationRepository, "findByName")
-        .mockResolvedValue(recommendation);
-      expect(recommendationService.insert(body)).rejects.toEqual({
-        message: "Recommendations names must be unique",
-        type: "conflict",
-      });
-    });
-  });
-
-  // --------------------------- UPVOTE RECOMMENDATION---------------------------
-
-  describe("Upvote Recommendation - POST /recommendation/:id/upvote", () => {
-    it("should return status 404 (not found) when a invalid recommendationId is sent", async () => {
-      const id = 0;
-      jest.spyOn(recommendationRepository, "find").mockResolvedValue(null);
-      expect(recommendationService.upvote(id)).rejects.toEqual({
-        message: "",
-        type: "not_found",
-      });
-    });
-  });
-
-  // --------------------------- DOWNVOTE RECOMMENDATION---------------------------
-
-  describe("Downvote Recommendation - POST /recommendation/:id/downvote", () => {
+describe('UNIT TESTING', () => {
     beforeEach(() => {
-      jest.clearAllMocks();
-      jest.resetAllMocks();
-    });
-    it("should return status 404 (not found) when a invalid recommendationId is sent", async () => {
-      const id = 0;
-      jest.spyOn(recommendationRepository, "find").mockResolvedValue(null);
-      expect(recommendationService.downvote(id)).rejects.toEqual({
-        message: "",
-        type: "not_found",
-      });
-    });
+        jest.clearAllMocks()
+        jest.resetAllMocks()
+    })
 
-    it("should remove a recommendation when a score becomes equal to less than -5", async () => {
-      const recommendation = await createMockRecommendation(-6);
+    // --------------------------- INSERT RECOMMENDATION---------------------------
 
-      jest
-        .spyOn(recommendationRepository, "find")
-        .mockResolvedValue(recommendation);
+    describe('Create Recommendation - POST /recommendation ', () => {
+        it('should return status 409(conflict) when an recommendation name is already registered (unique)', async () => {
+            const recommendation = await createMockRecommendation(0)
+            const body = {
+                name: recommendation.name,
+                youtubeLink: recommendation.youtubeLink,
+            }
+            jest.spyOn(
+                recommendationRepository,
+                'findByName'
+            ).mockResolvedValue(recommendation)
+            expect(recommendationService.insert(body)).rejects.toEqual({
+                message: 'Recommendations names must be unique',
+                type: 'conflict',
+            })
+        })
+    })
 
-      const updatedScore = jest
-        .spyOn(recommendationRepository, "updateScore")
-        .mockResolvedValue(recommendation);
+    // --------------------------- UPVOTE RECOMMENDATION---------------------------
 
-      const remove = jest
-        .spyOn(recommendationRepository, "remove")
-        .mockResolvedValue(null);
+    describe('Upvote Recommendation - POST /recommendation/:id/upvote', () => {
+        it('should return status 404 (not found) when a invalid recommendationId is sent', async () => {
+            const id = 0
+            jest.spyOn(recommendationRepository, 'find').mockResolvedValue(null)
+            expect(recommendationService.upvote(id)).rejects.toEqual({
+                message: '',
+                type: 'not_found',
+            })
+        })
+    })
 
-      await recommendationService.downvote(recommendation.id);
-      expect(updatedScore).toHaveBeenCalledTimes(1);
-      expect(remove).toHaveBeenCalledTimes(1);
-    });
-  });
+    // --------------------------- DOWNVOTE RECOMMENDATION---------------------------
 
-  // --------------------------- GET RANDOM RECOMMENDATION---------------------------
+    describe('Downvote Recommendation - POST /recommendation/:id/downvote', () => {
+        beforeEach(() => {
+            jest.clearAllMocks()
+            jest.resetAllMocks()
+        })
+        it('should return status 404 (not found) when a invalid recommendationId is sent', async () => {
+            const id = 0
+            jest.spyOn(recommendationRepository, 'find').mockResolvedValue(null)
+            expect(recommendationService.downvote(id)).rejects.toEqual({
+                message: '',
+                type: 'not_found',
+            })
+        })
 
-  describe("Get random recommendation - GET /recommendations/random", () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-      jest.resetAllMocks();
-    });
+        it('should remove a recommendation when a score becomes equal to less than -5', async () => {
+            const recommendation = await createMockRecommendation(-6)
 
-    it("should return error 404 (not_found) when no random recommendations are found", async () => {
-      const randomValue = 0.6;
-      jest.spyOn(global.Math, "random").mockReturnValue(randomValue);
-      jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce([]);
-      expect(recommendationService.getRandom()).rejects.toEqual({
-        message: "",
-        type: "not_found",
-      });
-    });
+            jest.spyOn(recommendationRepository, 'find').mockResolvedValue(
+                recommendation
+            )
 
-    it("getScoreFilter -> should return lte when a score is greater than 0.7", () => {
-      const randomNumber = 1;
-      jest.spyOn(global.Math, "random").mockReturnValueOnce(randomNumber);
-      const getScoreFilter = recommendationService.getScoreFilter(randomNumber);
-      expect(getScoreFilter).toBe("lte");
-    });
+            const updatedScore = jest
+                .spyOn(recommendationRepository, 'updateScore')
+                .mockResolvedValue(recommendation)
 
-    it("getScoreFilter -> should return gt when a score is lower than 0.7", () => {
-      const randomNumber = 0.1;
-      jest.spyOn(global.Math, "random").mockReturnValueOnce(randomNumber);
-      const getScoreFilter = recommendationService.getScoreFilter(randomNumber);
-      expect(getScoreFilter).toBe("gt");
-    });
+            const remove = jest
+                .spyOn(recommendationRepository, 'remove')
+                .mockResolvedValue(null)
 
-    it("getByScore -> should return recommendations if recommendations.length is greater than 0", async () => {
-      const mockRecommendation = await createMockRecommendation(0);
-      jest
-        .spyOn(recommendationRepository, "findAll")
-        .mockResolvedValue([mockRecommendation]);
-      const getByScore = await recommendationService.getByScore("lte");
-      expect(getByScore).toEqual([mockRecommendation]);
-    });
-  });
-});
+            await recommendationService.downvote(recommendation.id)
+            expect(updatedScore).toHaveBeenCalledTimes(1)
+            expect(remove).toHaveBeenCalledTimes(1)
+        })
+    })
+
+    // --------------------------- GET RANDOM RECOMMENDATION---------------------------
+
+    describe('Get random recommendation - GET /recommendations/random', () => {
+        beforeEach(() => {
+            jest.clearAllMocks()
+            jest.resetAllMocks()
+        })
+
+        it('should return error 404 (not_found) when no random recommendations are found', async () => {
+            const randomValue = 0.6
+            jest.spyOn(global.Math, 'random').mockReturnValue(randomValue)
+            jest.spyOn(
+                recommendationRepository,
+                'findAll'
+            ).mockResolvedValueOnce([])
+            expect(recommendationService.getRandom()).rejects.toEqual({
+                message: '',
+                type: 'not_found',
+            })
+        })
+
+        it('getScoreFilter -> should return lte when a score is greater than 0.7', () => {
+            const randomNumber = 1
+            jest.spyOn(global.Math, 'random').mockReturnValueOnce(randomNumber)
+            const getScoreFilter =
+                recommendationService.getScoreFilter(randomNumber)
+            expect(getScoreFilter).toBe('lte')
+        })
+
+        it('getScoreFilter -> should return gt when a score is lower than 0.7', () => {
+            const randomNumber = 0.1
+            jest.spyOn(global.Math, 'random').mockReturnValueOnce(randomNumber)
+            const getScoreFilter =
+                recommendationService.getScoreFilter(randomNumber)
+            expect(getScoreFilter).toBe('gt')
+        })
+
+        it('getByScore -> should return recommendations if recommendations.length is greater than 0', async () => {
+            const mockRecommendation = await createMockRecommendation(0)
+            jest.spyOn(recommendationRepository, 'findAll').mockResolvedValue([
+                mockRecommendation,
+            ])
+            const getByScore = await recommendationService.getByScore('lte')
+            expect(getByScore).toEqual([mockRecommendation])
+        })
+    })
+})
